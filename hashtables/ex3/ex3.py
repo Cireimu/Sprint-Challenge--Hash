17993bda6a32b9f4b0f250606c219d00e9dd4313
Cireimu <ethanhoover3@gmail.com>
2020-05-08 10:39:57 -0700

    mvp reached?

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,17 +1,30 @@
 def intersection(arrays):
+    inter_dict = {}
+    result = []
 
-    """
-    YOUR CODE HERE
-    """
-
+    # first we break the array of arrays into seperate arrays
+    # then go through each item in an array
+    # if that item exists in the dict then add 1 to the value
+    # if that item doesnt exist yet then add that item as a key with a 1 as the value
+    # then go through the inter_dict and add any dict keys that have a value equal to the number of inner arrays to the result list
+    # this represents that this particular item appears in all the arrays
+    for a in arrays:
+        for b in a:
+            if inter_dict.get(b):
+                inter_dict[b] += 1
+            else:
+                inter_dict[b] = 1
+    for key, value in inter_dict.items():
+        if value > len(arrays) - 1:
+            result.append(key)
     return result
 
 
 if __name__ == "__main__":
     arrays = []
 
-    arrays.append(list(range(1000000,2000000)) + [1,2,3])
-    arrays.append(list(range(2000000,3000000)) + [1,2,3])
-    arrays.append(list(range(3000000,4000000)) + [1,2,3])
+    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,33 @@
+# create a dict
+# sort the list of items from positive to negative
+# goes through sorted list
+# checks if item is in dict
+# if no then add item as a key with value of None
+# if its in there then do nothing
+# once it comes to
+
+
 def has_negatives(a):
-
-    """
-    YOUR CODE HERE
-    """
-
-    return result
+    # num_dict = {}
+    # a.sort(reverse=True)
+    # print(a)
+    # for n in a:
+    #     if n == 0:
+    #         continue
+    #     if n > 0:
+    #         if num_dict.get(n):
+    #             continue
+    #         else:
+    #             num_dict[n] = None
+    #     else:
+    #         print('called')
+    #         if num_dict.get(abs(n)):
+    #             print('called again')
+    #             num_dict[n] = n
+    # print(num_dict)
+    # # return result
+    pass
 
 
 if __name__ == "__main__":
-    print(has_negatives([-1,-2,1,2,3,4,-4]))
+    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
